78390a2cc6bfaaf0c4b3294009a50e44d0b4fdb8
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-16 12:08:53 +0100

    add arbitrary storage slot

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
+
+import "./StorageSlot.sol";
 
 contract Storage {
     uint x = 90;
@@ -10,5 +12,7 @@ contract Storage {
     constructor() {
         testing[84] = 177;
         testing[23] = 20;
+
+        StorageSlot.getUint256Slot(keccak256("jaymgeth")).value = 486;
     }
 }
--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat')
-const { keccak256, hexZeroPad } = hre.ethers.utils
+const { keccak256, hexZeroPad, toUtf8Bytes } = hre.ethers.utils
 
-const addr = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const addr = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 
 async function main() {
   const value1 = await hre.ethers.provider.getStorageAt(addr, '0x0')
@@ -12,13 +12,23 @@ async function main() {
 
   console.log(`Value at storage position 1: ${parseInt(value2)}`)
 
-  const key = hexZeroPad(84, 32)
-  const baseSlot = hexZeroPad(0x2, 32).slice(2)
-  const slot = keccak256(key + baseSlot)
+  let key = hexZeroPad(84, 32)
+  let baseSlot = hexZeroPad(0x2, 32).slice(2)
+  let slot = keccak256(key + baseSlot)
 
   const valueMapping = await hre.ethers.provider.getStorageAt(addr, slot)
 
   console.log(`Value mapping at storage position 2: ${parseInt(valueMapping)}`)
+
+  slot = keccak256(toUtf8Bytes('jaymgeth'))
+
+  const arbitraryMapping = await hre.ethers.provider.getStorageAt(addr, slot)
+
+  console.log(
+    `Arbitrary mapping at storage position keccak256('jaymgeth'): ${parseInt(
+      arbitraryMapping
+    )}`
+  )
 }
 
 main().catch((error) => {
